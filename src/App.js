53e46318109188978e134fe856d8c658726bc397
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-09-01 18:58:56 +0600

    navbar modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-<Navbar></Navbar>
+      <Navbar>
+
+        <Routes>
+          <Route path="/" element={<Home></Home>} > </Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/service" element={<Services></Services>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
+      </Navbar>
+
     </div>
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { NavLink } from "react-router-dom"
 
-const Navbar = () => {
+const Navbar = ({ children }) => {
     return (
         <div class="drawer drawer-end">
             <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content flex flex-col">
 
-                <div class="w-full navbar bg-base-300">
+                <div class="w-full navbar bg-base-300 px-20">
                     <div class="flex-1 px-2 mx-2 text-2xl">Refreshing Live</div>
                     <div class="flex-none lg:hidden">
                         <label for="my-drawer-3" class="btn btn-square btn-ghost">
@@ -15,22 +16,29 @@ const Navbar = () => {
                     </div>
 
                     <div class="flex-none hidden lg:block">
-                        <ul class="menu menu-horizontal">
+                        <ul class="menu menu-horizontal gap-x-3">
 
-                            <li><a>Navbar Item 1</a></li>
-                            <li><a>Navbar Item 2</a></li>
+                            <li><NavLink to='/' className='rounded-lg'>Home</NavLink></li>
+                            <li><NavLink to={'/about'} className='rounded-lg'>About</NavLink></li>
+                            <li><NavLink to={'/service'} className='rounded-lg'>Services</NavLink></li>
+                            <li><NavLink to={'/contact'} className='rounded-lg'>Contact</NavLink></li>
+                            <li><NavLink to={'/login'} className='rounded-lg'>LogIn</NavLink></li>
                         </ul>
                     </div>
                 </div>
 
-                Content
+                {children}
+
             </div>
             <div class="drawer-side">
                 <label for="my-drawer-3" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 ">
 
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <li><NavLink to='/' className='rounded-lg'>Home</NavLink></li>
+                    <li><NavLink to={'/about'} className='rounded-lg'>About</NavLink></li>
+                    <li><NavLink to={'/service'} className='rounded-lg'>Services</NavLink></li>
+                    <li><NavLink to={'/contact'} className='rounded-lg'>Contact</NavLink></li>
+                    <li><NavLink to={'/login'} className='rounded-lg'>LogIn</NavLink></li>
 
                 </ul>
 
